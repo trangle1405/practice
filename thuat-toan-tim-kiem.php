85d73bbdc6799085513c73597abb092afdf00135
Trang Le <trangle1405@gmail.com>
2019-08-06 16:38:58 +0700

    thuat toan tim kiem

---
+++ thuat-toan-tim-kiem.php
@@ -0,0 +1,56 @@
+<?php
+
+function findNumberByBinarySearch($array, $x)
+{
+    $left = 0;
+    $right = count($array) - 1;
+    while ($left <= $right) {
+        $mid = floor(($left + $right) / 2);
+        if ($x === $array[$mid]) {
+            return $mid;
+        } else if ($x < $array[$mid]) {
+            $right = $mid - 1;
+        } else if ($x > $array[$mid]) {
+            $left = $mid + 1;
+        }
+    }
+    return -1;
+}
+
+function findNumberByBinarySearchdequy($array, $left, $right, $x)
+{
+    $mid = floor(($left + $right) / 2);
+
+    if ($left > $right) {
+        return -1;
+    }
+
+    if ($x === $array[$mid]) {
+        return $mid;
+    }
+    if ($x > $array[$mid]) {
+        return findNumberByBinarySearchdequy($array, $mid + 1, $right, $x);
+    }
+    if ($x < $array[$mid]) {
+        return findNumberByBinarySearchdequy($array, $left, $right - 1, $x);
+    }
+}
+
+function findNumberBySequentialSearch($array, $x){
+    for ($i = 0; $i<count($array); $i++){
+        if ($array[$i] === $x){
+            return $i;
+        }
+
+    }
+    return -1;
+}
+
+
+$array1 = [1, 6, 2, 82, 89, 4, 9, 4];
+//sort($array1);
+$left = 0;
+$right = count($array1) - 1;
+$x = 2;
+//echo(findNumberByBinarySearchdequy($array1, $left, $right, $x));
+echo (findNumberBySequentialSearch($array1,2 ));
